7814ee9b0c0e9fa56555d90c07e8a5a566fb0760
tayla55bory <tayla55bory@outlook.com>
2026-05-14 16:02:11 +0530

    Create Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => (
+  <header className="header">
+    <h1>PeachPass Expense Portal</h1>
+    <p>Manage your highway tolls effortlessly</p>
+  </header>
+);
+
+export default Header;
